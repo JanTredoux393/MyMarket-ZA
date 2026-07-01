70f9d19293ef41d43f4bfa0563ba574cb29e16af
JanTredoux393 <jantlive@gmail.com>
2026-07-01 21:52:30 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ error_reporting(0);
 
 define('SITE_NAME', 'MyMarket-ZA');
 define('SITE_URL', 'http://mymarket-za.great-site.net');
-define('DB_HOST', 'sql313.infinityfree.com');        // e.g. sql301.infinityfree.com
-define('DB_USER', 'if0_42297008');    // e.g. epiz_12345678_mymarket
-define('DB_PASS', 'rvDtvotS54');    // the password you set
-define('DB_NAME', 'if0_42297008_mymarket_za');    // e.g. epiz_12345678_mymarket
+define('DB_HOST', 'sql313.infinityfree.com');        
+define('DB_USER', 'if0_42297008');    
+define('DB_PASS', 'rvDtvotS54');   
+define('DB_NAME', 'if0_42297008_mymarket_za');   
 ?>
